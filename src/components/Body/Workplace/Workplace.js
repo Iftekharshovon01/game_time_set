850e92c27b112e14b1de9f2f8147d2f68800d7bd
Iftekhar <iftekharshovonseu17@gmail.com>
2022-09-29 23:43:51 +0600

    cart setting

--- src/components/Body/Workplace/Workplace.js
+++ src/components/Body/Workplace/Workplace.js
@@ -1,69 +1,69 @@
-// import React from 'react';
-// import { useEffect } from 'react';
-// import { useState } from 'react';
-// // import Swal from 'sweetalert2'
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+// import Swal from 'sweetalert2'
 
-// const Workplace = (props) => {
-//     const [breaktimes, setBreaktimes] = useState(0);
-//     const { carts } = props;
-// console.log(carts);
-//     let totalTime = 0;
-//     carts.forEach(cart => {
-//         totalTime = totalTime + cart.time;
-//     });
+const Workplace = (props) => {
+    const [breaktimes, setBreaktimes] = useState(0);
+    const { carts } = props;
+console.log(carts);
+    let totalTime = 0;
+    carts.forEach(cart => {
+        totalTime = totalTime + cart.time;
+    });
 
-//     const AddBreak = (event) => {
-//         setBreaktimes(event);
-//         localStorage.setItem('break-time', event);
-//     };
+    // const AddBreak = (event) => {
+    //     setBreaktimes(event);
+    //     localStorage.setItem('break-time', event);
+    // };
 
-//     useEffect(() => {
-//         const getLocalStore = localStorage.getItem('break-time');
-//         if (getLocalStore) {
-//             setBreaktimes(getLocalStore);
-//         }
+    useEffect(() => {
+        const getLocalStore = localStorage.getItem('break-time');
+        if (getLocalStore) {
+            setBreaktimes(getLocalStore);
+        }
 
-//     }, [breaktimes]);
+    }, [breaktimes]);
 
-//     // const Alert = () => {
-//     //     Swal.fire(
-//     //         'Good job!',
-//     //         'Task Completed!',
-//     //         'success'
-//     //     )
+    // const Alert = () => {
+    //     Swal.fire(
+    //         'Good job!',
+    //         'Task Completed!',
+    //         'success'
+    //     )
 
-//     return (
-//         <div className='workplace_conatiner'>
-//             <div>
-//                 <h3>Information</h3>
-//                 <p><strong>Name:</strong> Rahman</p>
-//                 <p><strong>Skills:</strong>Tailwind, JavaScript, React</p>
-//             </div>
-//             <div>
-//                 <h3>Add a Break</h3>
-//                 <div className='break-btn'>
-//                     <button onClick={(event) => AddBreak(event.target.innerText)}><strong>10</strong></button>
-//                     <button onClick={(event) => AddBreak(event.target.innerText)}><strong>20</strong></button>
-//                     <button onClick={(event) => AddBreak(event.target.innerText)}><strong>30</strong></button>
-//                     <button onClick={(event) => AddBreak(event.target.innerText)}><strong>40</strong></button>
-//                 </div>
-//             </div>
-//             <div className='game-details'>
-//                 <h3>Game Details</h3>
-//                 <div className='game-time'>
-//                     <h3>Game Time</h3>
-//                     <p>{totalTime} minute</p>
-//                 </div>
-//                 <div className='break-time'>
-//                     <h3>Break Time</h3>
-//                     <p>{breaktimes} Secounds</p>
-//                 </div>
-//                 <div className='complete-btn'>
-//                     <button onClick={"Done"}>Game Completed</button>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+    return (
+        <div className='workplace_conatiner'>
+            {/* <div>
+                <h3>Information</h3>
+                <p><strong>Name:</strong> Rahman</p>
+                <p><strong>Skills:</strong>Tailwind, JavaScript, React</p>
+            </div> */}
+            {/* <div>
+                <h3>Add a Break</h3>
+                <div className='break-btn'>
+                    <button onClick={(event) => AddBreak(event.target.innerText)}><strong>10</strong></button>
+                    <button onClick={(event) => AddBreak(event.target.innerText)}><strong>20</strong></button>
+                    <button onClick={(event) => AddBreak(event.target.innerText)}><strong>30</strong></button>
+                    <button onClick={(event) => AddBreak(event.target.innerText)}><strong>40</strong></button>
+                </div>
+            </div> */}
+            {/* <div className='game-details'>
+                <h3>Game Details</h3>
+                <div className='game-time'>
+                    <h3>Game Time</h3>
+                    <p>{totalTime} minute</p>
+                </div>
+                <div className='break-time'>
+                    <h3>Break Time</h3>
+                    <p>{breaktimes} Secounds</p>
+                </div>
+                <div className='complete-btn'>
+                    <button onClick={"Done"}>Game Completed</button>
+                </div>
+            </div> */}
+        </div>
+    );
+};
 
-// export default Workplace;
+export default Workplace;
