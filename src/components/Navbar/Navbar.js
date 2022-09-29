9301d3a452c70d0ff61e79e5c39033480f0bb8c5
Iftekhar <iftekharshovonseu17@gmail.com>
2022-09-29 23:41:09 +0600

    nav complete

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import './Navbar.css';
-// import logo from '../../images/logo.png'
+import React from 'react';
+import './Navbar.css';
+import logo from '../../images/logo.png'
 
-// const Navbar = () => {
-//     return ( 
-//         <div className='navbar-container'>
-//             <img src={logo} alt="" />
-//             <h1>Games we love!</h1>
-//         </div>
-//     );
-// };
+const Navbar = () => {
+    return ( 
+        <div className='navbar-container'>
+            <img src={logo} alt="" />
+            <h1>Games we love!</h1>
+        </div>
+    );
+};
 
-// export default Navbar;
+export default Navbar;
